d4053a0bc2e7716184b62151eb10d1677fbbaee2
squidcarroll <squidcarroll@gmail.com>
2018-03-26 23:05:57 +0000

    push updated user

--- models/user.py
+++ models/user.py
@@ -2,14 +2,16 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    __tablename__ = "users"
