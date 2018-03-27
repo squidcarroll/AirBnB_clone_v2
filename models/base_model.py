69dd89e9ee10a5e2122a3df13c624a29f206466f
squidcarroll <squidcarroll@gmail.com>
2018-03-27 22:39:08 +0000

    more base stuff

--- models/base_model.py
+++ models/base_model.py
@@ -69,14 +69,15 @@ class BaseModel:
             Return dictionary representation of BaseModel class.
         '''
         cp_dct = dict(self.__dict__)
-        cp_dct['__class__'] = self.__class__.__name__
-        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
 
         if '_sa_instance_state' in cp_dct:
             del cp_dct['_sa_instance_state']
 
+        cp_dct['__class__'] = self.__class__.__name__
+        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
         return (cp_dct)
 
     def delete(self):
-        models.storage.delete()
+        models.storage.delete(self)
