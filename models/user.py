dfc638efb59decb9ebc80c409337925e3f6b36c6
Raid55 <boutboulnicholas@gmail.com>
2018-03-27 01:43:51 +0000

    sid fixed his dumbo mistakes

--- models/user.py
+++ models/user.py
@@ -4,15 +4,16 @@
 '''
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    places = relationship('Place', backref='users', cascade='delete')
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    __tablename__ = "users"
+    places = relationship('Place', backref='users', cascade='delete')
