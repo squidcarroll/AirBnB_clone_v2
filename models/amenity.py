f0b38034feed168b69d4c88957da092eabbc0933
squidcarroll <squidcarroll@gmail.com>
2018-03-27 21:23:08 +0000

    push 10

--- models/amenity.py
+++ models/amenity.py
@@ -14,6 +14,6 @@ class Amenity(BaseModel):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("place_amenity",
+    place_amenities = relationship("PlaceAmenity",
                                    backref="amenities",
                                    cascade="delete")
--- models/place.py
+++ models/place.py
@@ -10,18 +10,18 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 
-place_amenity = Table("place_amenity", Base.metadata,
-    Column('place_id',
-           String(60),
-           ForeignKey("places.id"),
-           primary_key=True,
-           nullable=False),
-    Column('amenity_id',
-           String(60),
-           ForeignKey("amenities.id"),
-           primary_key=True,
-           nullable=False)
-)
+class PlaceAmenity(Base):
+    __tablename__ = "place_amenity"
+    place_id = Column(String(60),
+                      ForeignKey("places.id"),
+                      primary_key=True,
+                      nullable=False),
+    amenity_id = Column(String(60),
+                        ForeignKey("amenities.id"),
+                        primary_key=True,
+                        nullable=False)
+    metadata = Base.metadata
+
 
 class Place(BaseModel, Base):
     '''
