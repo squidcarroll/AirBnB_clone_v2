29fd9c2368d2a45f22820f785f83706c001c1faf
squidcarroll <squidcarroll@gmail.com>
2018-03-27 00:53:10 +0000

    fix user

--- models/user.py
+++ models/user.py
@@ -12,6 +12,6 @@ class User(BaseModel, Base):
     '''
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
     __tablename__ = "users"
