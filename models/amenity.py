56ed065640e40a2614eff615eaa15cb4104f6a65
Raid55 <boutboulnicholas@gmail.com>
2018-03-29 17:12:49 +0000

    prev commit

--- models/amenity.py
+++ models/amenity.py
@@ -2,15 +2,11 @@
 '''
     Implementation of the Amenity class
 '''
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
-    __tablename__ = "amenities"
-
-    name = Column(String(128), nullable=False)
+    name = ""
