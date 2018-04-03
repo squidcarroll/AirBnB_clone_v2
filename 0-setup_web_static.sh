d5bc1464af0a4aeec9b24518ea7690f6f5fc6b02
squidcarroll <squidcarroll@gmail.com>
2018-04-03 22:50:28 +0000

    fix persmissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,8 @@
 
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
